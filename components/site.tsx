725c93771d2e60ebf39531fc26be01ecf09a9688
Charles Watkins <YOUR_GITHUB_EMAIL>
2026-04-25 20:20:29 -0500

    added app files

---
+++ components/site.tsx
@@ -0,0 +1,7 @@
+export default function Site() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-center">
+      <h1 className="text-5xl font-bold">WMD Website Live</h1>
+    </main>
+  );
+}
